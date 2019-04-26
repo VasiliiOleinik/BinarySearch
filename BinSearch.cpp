9ba274370e92e7f40d99e78663762330d22debc7
Vasilii <goooneoooogle@gmail.com>
2019-04-26 11:02:00 +0300

    Update BinSearch.cpp

--- BinSearch.cpp
+++ BinSearch.cpp
@@ -1,5 +1,5 @@
-// BinSearch.cpp: определяет точку входа для консольного приложения.
-//
+// BinSearch.cpp:
+
 #include "stdafx.h"
 #include <iostream>
 #include <iomanip>
@@ -16,62 +16,75 @@ int main()
 	int a[N];
 	int i;
 
-	// Заполнение случайными числамиx
-	for(i = 0; i < N; i++) a[i] = A + rand() % (B - A + 1);
+	// Filling with random numbers
+	for (i = 0; i < N; i++)
+		a[i] = A + rand() % (B - A + 1);
 
-	// Сортировка методом пузырька
-	for(i = 0; i < N; i++) {
-		for(int j = 0; j < N - i - 1; j++) 
-			if (a[j] > a[j+1]) swap(a[j], a[j+1]);
+	// Bubble sort
+	for (i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N - i - 1; j++)
+			if (a[j] > a[j + 1])
+				swap(a[j], a[j + 1]);
 	} // for i
 
-	// Вывод массива на экран
-	cout << "\nОтсортированный массив:\n";
-	for(i = 0; i < N; i++) {
+	// Array output to sceen
+	cout << "\nSorted array:\n";
+	for (i = 0; i < N; i++)
+	{
 		cout << setw(7) << a[i];
-		if ((i+1) % M == 0) cout << "\n";
+		if ((i + 1) % M == 0)
+			cout << "\n";
 	} // for i
-	if (N%M) cout << "\n";
+	if (N % M)
+		cout << "\n";
 
-	// Бинарный поиск
+	// Binary earch
 	int left, right, middle;
 	int key;
 	bool flag;
 
-	while(true) {
-		cout << "\nВведите число, которое надо найти в массиве (-999 для выхода)> ";
+	while (true)
+	{
+		cout << "\nEnter the number to be found in the array (-999 to exit)> ";
 		cin >> key;
-		if (key == -999) break;
+		if (key == -999)
+			break;
 
-		left = 0; right = N - 1; flag = false;
-		while(right >= left) {
-			middle = left + (right - left)/2;
-			if (a[middle] == key) {
+		left = 0;
+		right = N - 1;
+		flag = false;
+		while (right >= left)
+		{
+			middle = left + (right - left) / 2;
+			if (a[middle] == key)
+			{
 				flag = true;
 				break;
 			} // if
 
 			if (a[middle] < key)
 				left = middle + 1;
-			else 
+			else
 				right = middle - 1;
 		} // while
 
-		if (flag) {
-			cout << "Элемент со значением " << key << " найден в массиве :)\n"
-				<< "его индекс = " << middle << "\n";
-			// Вывод всех элементов, значение которых совпадает с ключом поиска
+		if (flag)
+		{
+			cout << "Element with value " << key << " found in array :)\n"
+				 << "his index = " << middle << "\n";
+			// Display all elements whose value matches the search keys
 			cout << "i\ta[i]\n--------------------------\n";
-			for (i = middle; a[i] == key; i++) {
+			for (i = middle; a[i] == key; i++)
+			{
 				cout << i << "\t" << a[i] << "\n";
 			} // for i
-		} 
+		}
 		else
 		{
-			cout << "Элемент со значением " << key << " не найден в массиве :(\n";
+			cout << "Element with value " << key << " found in array :)\n"
 		} // if
-	} // while
+	}	 // while
 
 	return 0;
 } // main
-
